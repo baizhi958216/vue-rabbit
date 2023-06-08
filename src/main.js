73450ba53a3f72d06cd7778958751bdf520cd744
baizhi958216 <1475289190@qq.com>
2023-06-09 00:10:58 +0800

    feat: 图片懒加载实现
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import { useIntersectionObserver } from "@vueuse/core";
 
 const app = createApp(App);
 
@@ -12,3 +13,16 @@ app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
+
+app.directive("img-lazy", {
+  mounted(el, binding) {
+    // el: 指令绑定的元素
+    // binding: binding.value 指令绑定表达式的值
+    useIntersectionObserver(el, ([{ isIntersecting }]) => {
+      if (isIntersecting) {
+        // 图片进入视口区
+        el.src = binding.value;
+      }
+    });
+  },
+});
