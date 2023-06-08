9d324964bb7b3bb72d0ecd32ea524f4d9dc08ceb
baizhi958216 <1475289190@qq.com>
2023-06-08 19:22:42 +0800

    feat: 整体路由设计
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "@/views/Login/index.vue";
+import Layout from "@/views/Layout/index.vue";
+import Home from "@/views/Home/index.vue";
+import Category from "@/views/Category/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    }
-  ]
-})
+      path: "/",
+      component: Layout,
+      children: [
+        {
+          path: "",
+          component: Home,
+        },
+        {
+          path: "category",
+          component: Category,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      component: Login,
+    },
+  ],
+});
 
-export default router
+export default router;
--- src/views/HomeView.vue
+++
@@ -1,5 +0,0 @@
-<script setup></script>
-
-<template>
-  <el-button type="primary">2023黑马程序员小兔鲜商城项目</el-button>
-</template>
