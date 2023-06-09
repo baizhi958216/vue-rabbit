2fe2ee70509f0984095b98a94837a40afc752d18
baizhi958216 <1475289190@qq.com>
2023-06-09 09:12:12 +0800

    feat: 面包屑导航实现
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,4 +1,17 @@
-<script setup></script>
+<script setup>
+import { getCategoryFilterAPI } from "@/apis/category";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+const categoryData = ref({});
+const route = useRoute();
+const getCattegoryData = async () => {
+  const res = await getCategoryFilterAPI(route.params.id);
+  categoryData.value = res.result;
+};
+onMounted(() => {
+  getCattegoryData();
+});
+</script>
 
 <template>
   <div class="container">
@@ -6,8 +19,11 @@
     <div class="bread-container">
       <el-breadcrumb separator=">">
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-        <el-breadcrumb-item :to="{ path: '/' }">居家 </el-breadcrumb-item>
-        <el-breadcrumb-item>居家生活用品</el-breadcrumb-item>
+        <el-breadcrumb-item
+          :to="{ path: `/category/${categoryData.parentId}` }"
+          >{{ categoryData.parentName }}</el-breadcrumb-item
+        >
+        <el-breadcrumb-item>{{ categoryData.name }}</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
     <div class="sub-container">
