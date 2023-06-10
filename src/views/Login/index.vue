2156dc308539aaeb58d7328684e4d0b1fa311463
baizhi958216 <1475289190@qq.com>
2023-06-10 20:16:08 +0800

    feat: 登录-pinia处理用户数据
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { ref } from "vue";
-import { loginAPI } from "@/apis/user";
 import { ElMessage } from "element-plus";
 import "element-plus/theme-chalk/el-message.css";
 import { useRouter } from "vue-router";
+import { useUserStore } from "@/stores/user";
+
+const userStore = useUserStore();
 
 // 表单校验(账号名+密码)
 
@@ -49,7 +51,7 @@ const doLogin = () => {
   formRef.value.validate(async (valid) => {
     // 所有表单都通过检验才为true
     if (valid) {
-      const res = await loginAPI({ account, password });
+      await userStore.getUserInfo({ account, password });
       // 1. 提示用户
       ElMessage({ type: "success", message: "登录成功" });
       // 2. 跳转首页
