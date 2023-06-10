a83089506f826d7cb28b57f77b0bd315693d0975
baizhi958216 <1475289190@qq.com>
2023-06-10 11:00:08 +0800

    feat: 图片预览组件-放大镜
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { useMouseInElement } from "@vueuse/core";
+import { watch } from "vue";
 import { ref } from "vue";
 
 // 图片列表
@@ -16,6 +18,37 @@ const activeIndex = ref(0);
 const enterhandle = (index) => {
   activeIndex.value = index;
 };
+
+// 获取鼠标相对位置
+const target = ref(null);
+const left = ref(0);
+const top = ref(0);
+
+const { elementX, elementY } = useMouseInElement(target);
+
+watch([elementX, elementY], () => {
+  // 横向
+  if (elementX.value > 100 && elementY.value < 300) {
+    left.value = elementX.value - 100;
+  }
+  // 纵向
+  if (elementY.value > 100 && elementX.value < 300) {
+    top.value = elementY.value - 100;
+  }
+  // 边界
+  if (elementX.value > 300) {
+    left.value = 200;
+  }
+  if (elementX.value < 100) {
+    left.value = 0;
+  }
+  if (elementY.value > 300) {
+    top.value = 200;
+  }
+  if (elementY.value < 100) {
+    top.value = 0;
+  }
+});
 </script>
 
 <template>
@@ -24,7 +57,7 @@ const enterhandle = (index) => {
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
