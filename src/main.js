0fac077de0a7420de63b90754fa4871a1bb896fb
baizhi958216 <1475289190@qq.com>
2023-06-10 12:14:47 +0800

    feat: 详情页全局组件插件化
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/main.js
+++ src/main.js
@@ -8,10 +8,14 @@ import router from "./router";
 
 import { lazyPlugin } from "@/directives";
 
+// 引入全局组件插件
+import { componentPlugin } from "@/components";
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 app.use(lazyPlugin);
+app.use(componentPlugin);
 
 app.mount("#app");
--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,7 +1,5 @@
 <script setup>
 import { getDetail } from "@/apis/detail";
-import DetailHot from "./components/DetailHot.vue";
-import ImageView from "@/components/ImageView/index.vue";
 import XtxSku from "@/components/XtxSku/index.vue";
 import { onMounted } from "vue";
 import { ref } from "vue";
@@ -50,7 +48,7 @@ const skuChange = (sku) => {
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-              <ImageView :image-list="goods.mainPictures" />
+              <XtxImageView :image-list="goods.mainPictures" />
 
               <!-- 统计数量 -->
               <ul class="goods-sales">
