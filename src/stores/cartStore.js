c4c34fb01a0e0cf7a86d3cc9513a80e721f69b96
baizhi958216 <1475289190@qq.com>
2023-06-16 14:36:13 +0800

    feat: 购物车统计数据
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -40,6 +40,20 @@ export const useCartStore = defineStore(
       cartList.value.forEach((item) => (item.selected = selected));
     };
 
+    // 已选择数量
+    const selectedCount = computed(() =>
+      cartList.value
+        .filter((item) => item.selected)
+        .reduce((a, c) => a + c.count, 0)
+    );
+
+    // 已选择商品价钱合计
+    const selectedPrice = computed(() =>
+      cartList.value
+        .filter((item) => item.selected)
+        .reduce((a, c) => a + c.count * c.price, 0)
+    );
+
     return {
       cartList,
       addCart,
@@ -49,6 +63,8 @@ export const useCartStore = defineStore(
       singleCheck,
       isAll,
       allCheck,
+      selectedCount,
+      selectedPrice,
     };
   },
   {
