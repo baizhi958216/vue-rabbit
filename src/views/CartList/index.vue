97ba834330bef95bccddf479992912bea6b413e7
baizhi958216 <1475289190@qq.com>
2023-06-24 20:11:52 +0800

    feat: 结算页路由配置以及数据渲染
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/views/CartList/index.vue
+++ src/views/CartList/index.vue
@@ -19,10 +19,7 @@ const allCheck = (selected) => {
           <thead>
             <tr>
               <th width="120">
-                <el-checkbox
-                  :model-value="cartStore.isAll"
-                  @change="allCheck"
-                />
+                <el-checkbox :model-value="cartStore.isAll" @change="allCheck" />
               </th>
               <th width="400">商品信息</th>
               <th width="220">单价</th>
@@ -36,16 +33,11 @@ const allCheck = (selected) => {
             <tr v-for="i in cartStore.cartList" :key="i.id">
               <td>
                 <!-- 单选框 -->
-                <el-checkbox
-                  :model-value="i.selected"
-                  @change="(selected) => singleCheck(i, selected)"
-                />
+                <el-checkbox :model-value="i.selected" @change="(selected) => singleCheck(i, selected)" />
               </td>
               <td>
                 <div class="goods">
-                  <RouterLink to="/"
-                    ><img :src="i.picture" alt=""
-                  /></RouterLink>
+                  <RouterLink to="/"><img :src="i.picture" alt="" /></RouterLink>
                   <div>
                     <p class="name ellipsis">
                       {{ i.name }}
@@ -64,12 +56,7 @@ const allCheck = (selected) => {
               </td>
               <td class="tc">
                 <p>
-                  <el-popconfirm
-                    title="确认删除吗?"
-                    confirm-button-text="确认"
-                    cancel-button-text="取消"
-                    @confirm="delCart(i)"
-                  >
+                  <el-popconfirm title="确认删除吗?" confirm-button-text="确认" cancel-button-text="取消" @confirm="delCart(i)">
                     <template #reference>
                       <a href="javascript:;">删除</a>
                     </template>
@@ -97,7 +84,7 @@ const allCheck = (selected) => {
           <span class="red">¥ {{ cartStore.selectedPrice.toFixed(2) }} </span>
         </div>
         <div class="total">
-          <el-button size="large" type="primary">下单结算</el-button>
+          <el-button size="large" type="primary" @click="$router.push('/checkout')">下单结算</el-button>
         </div>
       </div>
     </div>
@@ -182,7 +169,7 @@ const allCheck = (selected) => {
       height: 100px;
     }
 
-    > div {
+    >div {
       width: 280px;
       font-size: 16px;
       padding-left: 10px;
